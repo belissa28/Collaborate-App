03fd56dd26c2c553fb0a01a6c33c94a6d92652dd
belissa28 <41009713+belissa28@users.noreply.github.com>
2025-05-09 15:33:40 -0400

    Initial Commit

---
+++ Collaborate-App/Collaborate_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Collaborate_AppApp.swift
+//  Collaborate-App
+//
+//  Created by Belissa on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Collaborate_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
