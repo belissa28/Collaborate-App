8de530b3eb0257c9ee4d8907f2c4923f73db3c17
belissa28 <41009713+belissa28@users.noreply.github.com>
2025-05-09 17:24:07 -0400

    Added code for login page.

--- Collaborate-App/ContentView.swift
+++ Collaborate-App/ContentView.swift
@@ -8,9 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isTapped = false
+    //@State private var contentcreator: String = ""
+    //@State private var businessowner: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            LinearGradient(
+                            gradient: Gradient(colors: [.purple, .blue]),
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing)
+                        .ignoresSafeArea()
+            VStack{
+            // Set the background color here
+            
+            Text("Collab")
+                
+                //.font(.title)
+                .font(.system(size:60))
+                .foregroundColor(.white) // Optional: change text color for contrast
+                .bold()
+                .padding()
+            //Spacer().frame(height: 5)
+            Text("Sign up")
+                
+                //.font(.headline)
+                .font(.system(size:25))
+                .foregroundColor(.white) // Optional: change text color for contrast
+                //.padding()
+            Text("I am a...").foregroundColor(.white)
+            VStack(spacing: 16) {
+                Button("Content Creator") {
+                    print("Content Creator tapped")
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.white)
+                .foregroundColor(.black)
+                .cornerRadius(10)
+
+                Button("Business Owner") {
+                    print("Business Owner tapped")
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.white)
+                .foregroundColor(.black)
+                .cornerRadius(10)
+            }
+            .padding(.horizontal, 30)//            TextField("Content Creator", text: $contentcreator)
+//                .textFieldStyle(RoundedBorderTextFieldStyle())
+//            TextField("Business Owner", text: $businessowner)
+//                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            HStack{
+                Text("Already have an account?").foregroundColor(.white)
+                Text("Log in").underline().foregroundColor(isTapped ? .gray : .white).onTapGesture{
+                    isTapped.toggle()
+                    print("Log in tapped")
+                }
+            }
             .padding()
+          
+        }
+            
+        }
     }
 }
 
